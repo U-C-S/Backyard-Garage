40f78738d263e289efc3a4084e1c5c7fc657c6ab
U-C-S <ucs3721@outlook.com>
2021-05-16 10:33:15 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+
 const app = express();
-const port = 3000;
+const port = 7000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Node!");
 });
 
 app.listen(port, () => {
