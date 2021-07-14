4cfdeb2b104c7ec8e17d477314f2edfc3bcd1ef9
U-C-S <ucs3721@outlook.com>
2021-07-14 15:36:27 +0530

    Add nodemon for auto restart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
 
 const app = express();
 const port = 7000;
@@ -7,8 +7,8 @@ const port = 7000;
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname,"./index.html"))
 // });
-app.listen(process.env.PORT || 7000,() => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 7000, () => {
+  console.log(`listening at http://localhost:${port}`);
 });
 
 // app.listen(port, () => {
@@ -19,22 +19,29 @@ app.all("*", (req, res, next) => {
   let reqPath = req.url;
   let stringx = reqPath.slice(1).split("/");
   let info;
-  for (let i = 0; i < stringx.length; i++) {
-    const word = stringx[i];
-    if (i == 0) {
-      info = randomInfo[word];
-    } else {
-      info = info[word];
+
+  if (stringx[0]) {
+    for (let i = 0; i < stringx.length; i++) {
+      const word = stringx[i];
+      if (i == 0) {
+        info = randomInfo[word];
+      } else {
+        info = info[word];
+      }
     }
+  } else {
+    info = randomInfo;
   }
-  res.send(info);
+
+  res.json(info);
+  next();
 });
 
 let randomInfo = {
   animal: [
-    { name: "dog", legs: 4 },
+    { name: "doggy", legs: 4 },
     { name: "cow", legs: 4 },
     { name: "godzilla", legs: 4 },
   ],
-  where: "Earth",
+  where: "Earthling",
 };
